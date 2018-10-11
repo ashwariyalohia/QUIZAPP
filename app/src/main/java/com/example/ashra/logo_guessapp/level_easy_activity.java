7b2f723886ac2c674e6c8b432bbe37e3c24e9a0e
Ahmed Baig <ahmedbaig54@yahoo.com>
2018-10-11 09:01:12 +0500

    app quiz part complete

--- app/src/main/java/com/example/ashra/logo_guessapp/level_easy_activity.java
+++ app/src/main/java/com/example/ashra/logo_guessapp/level_easy_activity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -40,28 +42,19 @@ public class level_easy_activity extends AppCompatActivity {
     private Button answer_btn2;
     private Button answer_btn3;
     private Button answer_btn4;
+    private ImageView easyImage;
 
     private String right_answer;
     private int rightAnswerCount=0;
-    private int quizCount =1;
+    private int quizCount = 0;
     private FirebaseDatabase database;
 
-    static final private  int QUIZ_COUNT = 5;
+    static final private int QUIZ_COUNT = 9;
 
     ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
 
     ArrayList<Question>questions = new ArrayList<>();
 
-    String quizData[][] = {
-            //{"country ","Right_answer", "choice1" , "choice2" , "choice3" }
-            {"india","china","america","lahore","canada"},
-            {"lahore","lahaa","india","islamabad","karachi"},
-            {"karachi","amdabad","germany","rome","paris"},
-            {"itlay","city","saddar","ottaw","lodon" },
-            {"US","cuba","insta","fb","kashmir"},
-            {"india","havana","france","sir syed","nazimabad"},
-            {"itlay","america","lahore","canada","mukka"}
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,21 +69,7 @@ public class level_easy_activity extends AppCompatActivity {
         answer_btn3=(Button)findViewById(R.id.answer_btn3);
         answer_btn4=(Button)findViewById(R.id.answer_btn4);
 
-
-        //create quizArray from quizData
-//        for(int i = 0 ; i<quizData.length ; i++){
-//
-//            //prepare array
-//            ArrayList<String> tmpArray = new ArrayList<>();
-//            tmpArray.add(quizData[i][0]);  //country
-//            tmpArray.add(quizData[i][1]);   //cright answer
-//            tmpArray.add(quizData[i][2]);   //choice1
-//            tmpArray.add(quizData[i][3]);   //choice2
-//            tmpArray.add(quizData[i][4]);   //choice3
-//
-//            //add tempArray to quizArray
-//            quizArray.add(tmpArray);
-//        }
+        easyImage = findViewById(R.id.easyImage);
 
 
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -108,17 +87,118 @@ public class level_easy_activity extends AppCompatActivity {
                         JSONObject typeResponse = null;
                         JSONObject questionResponse = null;
                         JSONObject choicesResponse = null;
+                        JSONArray choices;
+                        String question;
+                        String rightAnswer;
+                        String image;
                         try {
                             jsonResponse = new JSONObject(response);
                             String type = jsonResponse.getString("easy"); // {easy:}
                             typeResponse = new JSONObject(type);
-                            String question = typeResponse.getString("q1"); //{q1:}
+
+                            question = typeResponse.getString("q1"); //{q1:}
                             questionResponse = new JSONObject(question);
-                            String rightAnswer = questionResponse.getString("right answer");
-                            String image = questionResponse.getString("image");
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
 //                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
-                            JSONArray choices = questionResponse.getJSONArray("choices");
+                            choices = questionResponse.getJSONArray("choices");
                             Question q1 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q1);
+//                            Toast.makeText(getApplicationContext(),questions.get(0).getImgUrl().toString(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q2"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q2 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q2);
+//                            Toast.makeText(getApplicationContext(),questions.get(1).getImgUrl().toString(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q3"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q3 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q3);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q4"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q4 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q4);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q5"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q5 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q5);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q6"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q6 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q6);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q7"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q7 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q7);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q8"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q8 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q8);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q9"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q9 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q9);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            question = typeResponse.getString("q10"); //{q1:}
+                            questionResponse = new JSONObject(question);
+                            rightAnswer = questionResponse.getString("right answer");
+                            image = questionResponse.getString("image");
+//                          Toast.makeText(getApplicationContext(), image, Toast.LENGTH_LONG).show();
+                            choices = questionResponse.getJSONArray("choices");
+                            Question q10 = new Question(image, type, rightAnswer, choices.getString(1), choices.getString(2), choices.getString(3), choices.getString(4));
+                            questions.add(q10);
+//                            Toast.makeText(getApplicationContext(),questions.size(), Toast.LENGTH_LONG).show();
+
+                            Collections.shuffle(questions);
+                            showNextQuiz();
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -133,78 +213,70 @@ public class level_easy_activity extends AppCompatActivity {
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
 
-//        showNextQuiz();
     }
 
-//    public void showNextQuiz() {
-//
-//       //generate random number between 0 and 14 (quizArray's size - 1)
-//        Random random=new Random();
-//        int randomNum = random.nextInt(quizArray.size());
-//
-//        //pick one quiz set
-//        ArrayList<String> quiz = quizArray.get(randomNum);
-//
-//        //set question and right answer
-//        //Array format:{"country","right answer" , "choice1" , "choice2" , "choice3"}
-//        question_label.setText(quiz.get(0));    //"quiz" name of array list for random number
-//        right_answer=quiz.get(1);
-//
-//        //remove "country" from quiz and shuffle  choice
-//        quiz.remove(0);
-//        Collections.shuffle(quiz);
-//
-//        //set choice
-//        answer_btn1.setText(quiz.get(0));
-//        answer_btn2.setText(quiz.get(1));
-//        answer_btn3.setText(quiz.get(2));
-//        answer_btn4.setText(quiz.get(3));
-//
-//        //remove this quiz from quizArray
-//        quizArray.remove(randomNum);
-//    }
-//    public void CheckAnswer(View view){
-//
-//    //get push button
-//        Button answerBtn = (Button) findViewById(view.getId());
-//        String btnText =answerBtn.getText().toString();
-//
-//        String alertTitle;
-//
-//        if (btnText.equals(right_answer)){  //chck from array format
-//
-//            //correct
-//            alertTitle = "CORRECT";
-//            rightAnswerCount++;
-//        }
-//        else {
-//            //wrong
-//            alertTitle="WRONG";
-//            }
-//
-//            //create Dialoge
-//        AlertDialog.Builder builder= new AlertDialog.Builder(this);
-//        builder.setTitle(alertTitle);
-//        builder.setMessage("Answer:  "+ right_answer);
-//        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
-//
-//                if(quizCount == QUIZ_COUNT){
-//                    //show Result
-//                    Intent intent = new Intent(getApplicationContext(),Result.class);   //score activity intent
-//                    intent.putExtra("RIGHT_ANSWER_COUNT",rightAnswerCount);
-//                    startActivity(intent);
-//                    finish();
-//                }
-//                else {
-//                    quizCount++;
-//                    showNextQuiz();
-//                }
-//            }
-//        });
-//
-//        builder.setCancelable(false);
-//        builder.show();
-//    }
+    public void showNextQuiz() {
+//        Toast.makeText(this,questions.get(0).getImgUrl().toString(), Toast.LENGTH_LONG).show();
+//        Toast.makeText(this,questions.get(quizCount).getImgUrl(), Toast.LENGTH_LONG).show();
+
+        Picasso.with(getApplicationContext()).load(questions.get(quizCount).getImgUrl().toString()).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(easyImage);
+        //set question and right answer
+
+        right_answer=questions.get(quizCount).getRightAnswer();
+
+        //remove "country" from quiz and shuffle  choice
+
+
+        //set choice
+        answer_btn1.setText(questions.get(quizCount).getChoice1().toString());
+        answer_btn2.setText(questions.get(quizCount).getChoice2().toString());
+        answer_btn3.setText(questions.get(quizCount).getChoice3().toString());
+        answer_btn4.setText(questions.get(quizCount).getChoice4().toString());
+
+        //remove this quiz from quizArray
+    }
+    public void CheckAnswer(View view){
+
+    //get push button
+        Button answerBtn = (Button) findViewById(view.getId());
+        String btnText =answerBtn.getText().toString();
+
+        String alertTitle;
+
+        if (btnText.equals(right_answer)){  //chck from array format
+
+            //correct
+            alertTitle = "CORRECT";
+            rightAnswerCount++;
+        }
+        else {
+            //wrong
+            alertTitle="WRONG";
+        }
+
+            //create Dialoge
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle);
+        builder.setMessage("Answer:  "+ right_answer);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if(quizCount == QUIZ_COUNT){
+                    //show Result
+                    Intent intent = new Intent(getApplicationContext(),Result.class);   //score activity intent
+                    intent.putExtra("RIGHT_ANSWER_COUNT",rightAnswerCount);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    quizCount++;
+                    showNextQuiz();
+                }
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.show();
+    }
 }
